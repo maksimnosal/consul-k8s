d05f90ce2423c677ea13631b9a25175688b9c1f4
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2023-03-13 22:01:22 -0500

    checkpoint

--- control-plane/api-gateway/translator.go
+++ control-plane/api-gateway/translator.go
@@ -1,85 +1,78 @@
 package apigateway
 
 import (
+	"fmt"
 	capi "github.com/hashicorp/consul/api"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/runtime"
 	gwv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
-	"time"
 )
 
-type Translator interface {
+type Translate interface {
 	//TODO not sure which k8s interface exactly makes sense to use at the moment, think it would be object
-	K8sObjectToConfigEntry(object client.Object) capi.ConfigEntry
-	ConfigEntryToK8sObject(configEntry capi.ConfigEntry)
+	K8sObjectToConfigEntry(object runtime.Object) (capi.ConfigEntry, error)
+	ConfigEntryToK8sObject(configEntry capi.ConfigEntry) (runtime.Object, error)
 }
 
 type Translator struct {
-	//TODO actually can probably get away with not having these clients at all and assume whatever is calling this knows things
-	//but might need these at some point when translating secrets (?)
-	//gatewayClient
-	//consulClient
+	consulClient capi.Client
 }
 
-// would be replaced with gatewayclient.GetGateway in actual port
-// TODO where should we put k8s controller helper files probably in the sub command
-func fakeGatewayK8s() *gwv1beta1.Gateway {
-	return &gwv1beta1.Gateway{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Gateway",
-			APIVersion: "v1beta",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:            "",
-			GenerateName:    "",
-			Namespace:       "",
-			UID:             "",
-			ResourceVersion: "",
-			Generation:      0,
-			CreationTimestamp: metav1.Time{
-				Time: time.Time{},
-			},
-			DeletionTimestamp: &metav1.Time{
-				Time: time.Time{},
-			},
-			DeletionGracePeriodSeconds: nil,
-			Labels:                     nil,
-			Annotations:                nil,
-			OwnerReferences:            nil,
-			Finalizers:                 nil,
-			ManagedFields:              nil,
-		},
-		Spec: gwv1beta1.GatewaySpec{
-			GatewayClassName: "test",
-			Listeners: []gwv1beta1.Listener{
-				{
-					"",
-					nil,
-					0,
-					"",
-					&gwv1beta1.GatewayTLSConfig{
-						Mode:            nil,
-						CertificateRefs: nil,
-						Options:         nil,
-					},
-					&gwv1beta1.AllowedRoutes{
-						Namespaces: &gwv1beta1.RouteNamespaces{
-							From: nil,
-							Selector: &metav1.LabelSelector{
-								MatchLabels:      nil,
-								MatchExpressions: nil,
-							},
-						},
-						Kinds: nil,
-					},
-				},
-			},
-			Addresses: nil,
-		},
-		Status: gwv1beta1.GatewayStatus{
-			Addresses:  nil,
-			Conditions: nil,
-			Listeners:  nil,
-		},
+func (t *Translator) K8sObjectToConfigEntry(object runtime.Object) (capi.ConfigEntry, error) {
+	//determine type of k8s object
+	switch object.(type) {
+	case *gwv1beta1.Gateway:
+		return t.gatewayK8sToConfig(object), nil
 	}
+	return nil, fmt.Errorf("invalid runtime.Object type")
+}
+
+func (t *Translator) ConfigEntryToK8sObject(configEntry capi.ConfigEntry) (runtime.Object, error) {
+	//determine type of k8s object
+	switch configEntry.(type) {
+	case *capi.APIGatewayConfigEntry:
+		return t.gatewayConfigToK8s(configEntry), nil
+	}
+	return nil, fmt.Errorf("invalid api.ConfigEntry type")
+}
+
+func (t *Translator) gatewayK8sToConfig(object runtime.Object) *capi.APIGatewayConfigEntry {
+	gateway := object.(*gwv1beta1.Gateway)
+	return &capi.APIGatewayConfigEntry{
+		Kind: capi.APIGateway,
+		Name: gateway.Name,
+		//TODO how do we account for enterprise
+		Namespace: "",
+		//TODO how does this value map?
+		Meta:      map[string]string{},
+		Listeners: t.listenersK8sToConfig(gateway.Spec.Listeners),
+	}
+}
+
+func (t *Translator) listenersK8sToConfig(listeners []gwv1beta1.Listener) []capi.APIGatewayListener {
+	output := make([]capi.APIGatewayListener, len(listeners))
+	for i, l := range listeners {
+		output[i] = capi.APIGatewayListener{
+			Name:     string(l.Name),
+			Hostname: string(*l.Hostname),
+			Port:     int(l.Port),
+			Protocol: string(l.Protocol),
+			TLS:      t.tlsK8sToConfig(l.TLS),
+		}
+	}
+	return output
+}
+
+func (t *Translator) tlsK8sToConfig(tls *gwv1beta1.GatewayTLSConfig) capi.APIGatewayTLSConfiguration {
+	return capi.APIGatewayTLSConfiguration{
+		Certificates: t.k8sCertificatesToConfig(tls.CertificateRefs),
+	}
+}
+
+// TODO this one will need a bit more work
+func (t *Translator) k8sCertificatesToConfig(certs []gwv1beta1.SecretObjectReference) []capi.ResourceReference {
+	return []capi.ResourceReference{}
+}
+
+func (t *Translator) gatewayConfigToK8s(configEntry capi.ConfigEntry) *gwv1beta1.Gateway {
+	return &gwv1beta1.Gateway{}
 }
