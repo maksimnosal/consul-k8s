4db7ad86593750c5255988bfb4d43cc259e5cbc8
Thomas Eckert <teckert@hashicorp.com>
2023-03-28 13:34:35 -0400

    Stub out gatewayclassconfig controller

--- control-plane/api-gateway/gatewayclassconfig/controller.go
+++ control-plane/api-gateway/gatewayclassconfig/controller.go
@@ -2,17 +2,21 @@ package gatewayclassconfig
 
 import (
 	"context"
+	"time"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	apigateway "github.com/hashicorp/consul-k8s/control-plane/api-gateway"
+	v1alpha1 "github.com/hashicorp/consul-k8s/control-plane/api/v1alpha1"
 	"github.com/hashicorp/go-hclog"
+)
+
+const (
+	gatewayClassConfigFinalizer = "gateway-class-exists-finalizer.api-gateway.consul.hashicorp.com"
 )
 
 // GatewayClassConfigController reconciles GatewayClassConfig resources.
 type GatewayClassConfigController struct {
-	Client apigateway.Client
-	Log    hclog.Logger
+	Log hclog.Logger
 }
 
 func NewGatewayClassController() *GatewayClassConfigController {
@@ -25,13 +29,50 @@ func NewGatewayClassController() *GatewayClassConfigController {
 func (g *GatewayClassConfigController) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := g.Log.With("gatewayClassConfig", req.NamespacedName)
 
-	gcc, err := g.Client.GetGatewayClassConfig(ctx, req.NamespacedName)
+	gcc, err := get(ctx, req.NamespacedName)
 	if err != nil {
 		logger.Error("error getting gateway class config", "err", err)
 		return ctrl.Result{}, err
 	}
 
+	if gcc == nil {
+		logger.Info("gateway class config deleted")
+		return ctrl.Result{}, nil
+	}
+
+	if !gcc.ObjectMeta.DeletionTimestamp.IsZero() {
+		inUse, err := inUse(ctx, gcc)
+		if err != nil {
+			logger.Error("error checking if gateway class config is in use", "err", err)
+			return ctrl.Result{}, err
+		}
+
+		if inUse {
+			logger.Trace("gateway class config is in use, not deleting")
+			return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+		}
+
+		if _, err := removeFinalizer(ctx, gcc); err != nil {
+			logger.Error("error removing finalizer", "err", err)
+			return ctrl.Result{}, err
+		}
+	}
+
 	logger.Info("reconciling gateway class config", "gatewayClassConfig", gcc)
 
 	return ctrl.Result{}, nil
+}
+
+func get(ctx context.Context, namespacedName any) (*v1alpha1.GatewayClassConfig, error) {
+	var gcc *v1alpha1.GatewayClassConfig
+
+	// This should actually be done by the client.
+
+	return gcc, nil
+}
+
+func inUse(ctx context.Context, gcc *v1alpha1.GatewayClassConfig) (bool, error) {
+	// This should actually be done by the client.
+
+	return false, nil
 }
