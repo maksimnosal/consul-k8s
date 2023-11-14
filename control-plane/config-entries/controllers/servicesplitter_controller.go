8f75ea0c0ff057f326f7afa70d62a8d5da8b8751
Derek Menteer <derek.menteer@hashicorp.com>
2023-11-14 10:34:20 -0600

    Temp commit for POC.

--- control-plane/config-entries/controllers/servicesplitter_controller.go
+++ control-plane/config-entries/controllers/servicesplitter_controller.go
@@ -5,7 +5,10 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
+	jsonpatch "github.com/evanphx/json-patch"
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -34,10 +37,63 @@ func (r *ServiceSplitterController) Logger(name types.NamespacedName) logr.Logge
 	return r.Log.WithValues("request", name)
 }
 
+func (r *ServiceSplitterController) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+	return r.Client.Patch(ctx, obj, AddFinalizerPatch(obj, "blarg"))
+	// return r.Client.Update(ctx, obj, opts...)
+}
+
 func (r *ServiceSplitterController) UpdateStatus(ctx context.Context, obj client.Object, opts ...client.SubResourceUpdateOption) error {
 	return r.Status().Update(ctx, obj, opts...)
 }
 
 func (r *ServiceSplitterController) SetupWithManager(mgr ctrl.Manager) error {
 	return setupWithManager(mgr, &consulv1alpha1.ServiceSplitter{}, r)
+}
+
+func (r *ServiceSplitterController) UpdateFinalizers(ctx context.Context, obj client.Object, opts ...client.PatchOption) error {
+	return r.Patch(ctx, obj, client.Apply, opts...)
+}
+
+var _ client.Patch = (*FinalizerPatch)(nil)
+
+func AddFinalizerPatch(oldObj client.Object, addFinalizers ...string) *FinalizerPatch {
+	output := make([]string, 0, len(addFinalizers))
+	existing := make(map[string]bool)
+	for _, f := range oldObj.GetFinalizers() {
+		existing[f] = true
+		output = append(output, f)
+	}
+	for _, f := range addFinalizers {
+		if !existing[f] {
+			output = append(output, f)
+		}
+	}
+	return &FinalizerPatch{
+		NewFinalizers: output,
+	}
+}
+
+type FinalizerPatch struct {
+	NewFinalizers []string
+}
+
+// Data implements client.Patch.
+func (fp *FinalizerPatch) Data(obj client.Object) ([]byte, error) {
+	newData, err := json.Marshal(map[string]any{
+		"metadata": map[string]any{
+			"finalizers": fp.NewFinalizers,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := jsonpatch.CreateMergePatch([]byte(`{}`), newData)
+	fmt.Println("============================", string(p))
+	return p, err
+}
+
+// Type implements client.Patch.
+func (fp *FinalizerPatch) Type() types.PatchType {
+	return types.MergePatchType
 }
